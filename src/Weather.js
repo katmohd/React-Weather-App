62ff494406abd4a528a14869ecbf05ee3c02834c
Kat <kartmohd@gmail.com>
2023-12-07 02:41:36 +0800

    installed weather components

---
+++ src/Weather.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import WEATHER_API_KEY from "./api";
+import WeatherInfo from "./WeatherInfo";
+import axios from "axios";
+import "./Weather.css";
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coord,
+      icon: response.data.weather[0].icon,
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      date: new Date(response.data.dt * 1000),
+      feels: Math.round(response.data.main.feels_like),
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      condition: response.data.weather[0].condition,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${WEATHER_API_KEY}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="container">
+        <div className="Weather">
+          <header>
+            <form onSubmit={handleSubmit} className="search-form">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control search-form-input"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+              <input
+                type="submit"
+                value="Search"
+                className="search-form-button"
+              />
+            </form>
+          </header>
+          <main>
+            <WeatherInfo data={weatherData} />
+          </main>
+          <footer>
+            This project was coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/katmohd/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Kat
+            </a>{" "}
+            and is{" "}
+            <a
+              href="https://github.com/katmohd/React-Weather-App"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub
+            </a>{" "}
+            and{" "}
+            <a
+              href="https://endearing-cucurucho-8053a9.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify
+            </a>
+          </footer>
+        </div>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
+}
---
+++ src/WeatherForecast.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from "react";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+import WEATHER_API_KEY from "./api";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast mt-3">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index > 0 && index < 6) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    load();
+
+    return null;
+  }
+}
